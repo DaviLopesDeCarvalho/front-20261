139fc6aedf304c67bbc840866fccd04520280364
DaviLopesDeCarvalho <davi.l.carvalho@iesb.edu.brgit config -listgit config --global user.email davi.l.carvalho@iesb.edu.br>
2026-03-23 10:29:22 -0300

    adicionando codigos pratica 1 e 2

--- aulas/aula01/src/main.js
+++ aulas/aula01/src/main.js
@@ -1,1 +1,22 @@
 import './style.css'
+
+
+const matricula = document.querySelector('#matricula');
+const senha = document.querySelector('#senha');
+const botao = document.querySelector('button');
+
+botao.addEventListener('click', function(e) {
+    const matriculaErro = document.querySelector('#matriculaErro')
+    const senhaErro = document.querySelector('#senhaErro')
+
+    matriculaErro.textContent = '';
+    senhaErro.textContent = '';
+
+
+    if (matricula.value == "") {
+        matriculaErro.textContent = "Matrícula é obrigatória";
+        return
+    }
+
+    
+})
