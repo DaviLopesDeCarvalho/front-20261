40878ce1a22818ef83d04c7bfa3f0795c374b9c2
DaviLopesDeCarvalho <davi.l.carvalho@iesb.edu.brgit config -listgit config --global user.email davi.l.carvalho@iesb.edu.br>
2026-03-24 10:54:16 -0300

    Adicionando codigos aula02

---
+++ aulas/aula02/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import logo from './assets/learn.svg';
+
+function App() {
+  return (
+    <main>
+      <img src={logo} alt="Logotipo da aplicação" />
+      <h1>Aluno Online</h1>
+      <label htmlFor="matricula">Matricula</label>
+      <input type="number" id="matricula" name="matricula" />
+      <p id='matriculaErro'></p>
+      <label htmlfor="senha">Senha</label>
+      <input type="password" id="senha" name="senha" />
+      <p id='senhaErro'></p>
+      <button type="submit">Entrar</button>
+    </main>
+  );
+}
+
+export default App;
