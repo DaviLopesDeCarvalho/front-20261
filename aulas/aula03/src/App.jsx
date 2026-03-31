b840a3c517dc4e883fb3962024b7a312fc6c456f
DaviLopesDeCarvalho <davi.l.carvalho@iesb.edu.brgit config -listgit config --global user.email davi.l.carvalho@iesb.edu.br>
2026-03-31 10:43:53 -0300

    adiciona codigos aula03

--- aulas/aula03/src/App.jsx
+++ aulas/aula03/src/App.jsx
@@ -1,60 +1,14 @@
-import logo from './assets/learn.svg'
-import avatar from './assets/avatar.svg'
+
+import Boletos from "./pages/Boletos";
+import Dashboard from "./pages/Dashboard";
+import Faltas from "./pages/Faltas";
+import Notas from "./pages/Notas";
+import Requerimentos from "./pages/Requirementos";
 
 function App() {
-
-  return <div className='flex flex-col md:flex-row'>
-    <aside className='hidden md:block bg-gray-300'>
-      <header className='flex flex-row'>
-        <img src={logo} alt="Logotipo Aluno Online" />
-        <h1>Aluno Online</h1>
-      </header>
-      <nav>
-        <ul>
-          <li>Dashboard</li>
-          <li>Notas</li>
-          <li>Faltas</li>
-          <li>Boletos</li>
-          <li>Requerimentos</li>
-          <li>Sair</li>
-        </ul>
-      </nav>
-    </aside>
-    <main className='flex flex-col max-w-screen'>
-      <header>
-        <h1>Olá, Aluno!</h1>
-        <img src={avatar} alt="imagem do avatar" />
-      </header>
-      <h2>Bem-vindo ao portal do aluno</h2>
-      <section>
-        <article>
-          <h3>Mural de Avisos</h3>
-          <ul>
-            <li>Inscrição para o projeto de extensão</li>
-            <li>Eleição para representante de turma</li>
-            <li></li>
-          </ul>
-        </article>
-        <article>
-          <h3>Calendário Acadêmico</h3>
-          <ul>
-            <li>23/02 - Início do período letivo 2026-1</li>
-            <li>25/04 - Prazo final para aplicação da P1</li>
-            <li>23/06 - Prazo final para aplicação da P2</li>
-            <li>04/07 - Fim do período letivo 2026-1</li>
-          </ul>
-        </article>
-        <article>
-          <h3>Minhas Disciplinas</h3>
-          <ul>
-            <li>BI e Data Warehousing</li>
-            <li>Construção de Frontend</li>
-            <li>Manutenção de Software e Devops</li>
-          </ul>
-        </article>
-      </section>
-    </main>
-  </div>
+  return ( 
+  <Dashboard />
+  );
 }
 
 export default App;
