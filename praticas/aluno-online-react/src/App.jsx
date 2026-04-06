4008bed0daf54d7cd854d9de3ad6f990260b12bf
DaviLopesDeCarvalho <davi.l.carvalho@iesb.edu.brgit config -listgit config --global user.email davi.l.carvalho@iesb.edu.br>
2026-04-06 10:31:30 -0300

    feat: conclui pratica03. Fecha #5

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,11 +1,7 @@
-import Boletos from "./pages/Boletos";
-import Dashboard from "./pages/Dashboard";
-import Faltas from "./pages/Faltas";
-import Notas from "./pages/Notas";
-import Requerimentos from "./pages/Requerimentos";
+import "./App.css";
 
 function App() {
-  return <Dashboard />;
+  return <></>;
 }
 
 export default App;
